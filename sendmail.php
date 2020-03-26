d05019463f63f99b8bb5ebd53dac649ca3ba1a2f
iakanoe <iakanoe1056@gmail.com>
2020-03-26 17:31:47 -0300

    arreglo todo lo que faltaba

--- sendmail.php
+++ sendmail.php
@@ -1,44 +1,12 @@
 <?php
-$to_email_address = "SQLemonrobotics@gmail.com";
-// Check if the form is submitted
-if ( isset( $_POST['submitbtn'] ) ) {
-//echo '<h2>form data retrieved by using the $_REQUEST variable<h2/>';
+if (isset($_POST['submitbtn'])) {
+    $Name = $_REQUEST['name'];
+    $Email = $_REQUEST['email'];
+    $Subject = $_REQUEST['subject'];
+    $Body = $_REQUEST['message'];
 
-$Name = $_REQUEST['name'];
-$Email = $_REQUEST['email'];
-$Subject = $_REQUEST['subject'];
-$Body = $_REQUEST['message'];
-
-$finalMessage = "mensaje de:"  . $Email . "\n\n" . $Body;
-// display the results
-echo 'Your name is ' . $Name .',' . $Email .','. $Subject .','. $Body .','. $finalMessage;
-mail($to_email_address,$Subject,$finalMessage);
-/*
-// check if the post method is used to submit the form
-
-if ( filter_has_var( INPUT_POST, 'submit' ) ) {
-
-echo '<h2>form data retrieved by using $_POST variable<h2/>'
-
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-
-// display the results
-echo 'Your name is ' . $lastname .' ' . $firstname;
+    $finalMessage = "Mensaje de: " . $Email . "\n\n" . $Body;
+    mail($to_email_address, $Subject, $finalMessage);
+    header('Location: index.html');
+    exit;
 }
-
-// check if the get method is used to submit the form
-
-if ( filter_has_var( INPUT_GET, 'submit' ) ) {
-
-echo '<h2>form data retrieved by using $_GET variable<h2/>'
-
-$firstname = $_GET['firstname'];
-$lastname = $_GET['lastname'];
-}
-
-// display the results
-echo 'Your name is ' . $lastname .' ' . $firstname;
-exit;*/
-}
-?>
